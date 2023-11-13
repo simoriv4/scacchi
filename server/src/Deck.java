971b0e835d2edc549dbff0ac155a31c40d996002
AlbertoMonti05 <20albymonti05@gmail.com>
2023-11-13 12:35:02 +0100

    eliminazione classe CardsDiscarded perchè gestisco le carte scartate come un mazzo. Trasferimento metodi da classe CardsDiscarded a classe Deck.

--- server/src/Deck.java
+++ server/src/Deck.java
@@ -470,4 +470,36 @@ public class Deck <E extends Card>  //<E extends Card> --> indica che la classe 
         
         //.............
     }
+
+
+
+
+
+
+    //INIZIO METODI PER MAZZO DEGLI SCARTI
+    /**
+     * metodo per ottenere il colore della carta in cima al mazzo degli scarti
+     * @return colore della carta in cima al mazzo degli scarti
+     */
+    public String getColorCardOnTop()
+    {
+        //la carta in cima al mazzo si trova all'ultima posizione della lista
+        return deck.get(deck.size() - 1).getColor();
+    }
+
+    /**
+     * metodo per ottenere il numero della carta in cima al mazzo degli scarti
+     * @return numero della carta in cima al mazzo degli scarti
+     */
+    public int getNumberCardOnTop()
+    {
+        //la carta in cima al mazzo si trova all'ultima posizione della lista
+        return deck.get(deck.size() - 1).getNumber();
+    }
+    //FINE METODI PER MAZZO DEGLI SCARTI
+
+
+    //INIZIO METODI PER MAZZO DEGLI UTENTI (carte che ha in mano)
+
+    //FINE METODI PER MAZZO DEGLI UTENTI (carte che ha in mano)
 }

4146d7cf39bc20a7ac573dbd87f89f3d0b1a9a07
AlbertoMonti05 <20albymonti05@gmail.com>
2023-11-20 12:08:53 +0100

    test comunicazione con pc

--- server/src/Game.java
+++ server/src/Game.java
@@ -94,7 +94,7 @@ public class Game {
                     if(isAvailable)
                     {
                         // // creo l'utente e lo aggiungo alla lista
-                        User u2 = new User(clientSocket.getPort(), "127.0.0.1", false, false, false, this.message.username);
+                        User u2 = new User(clientSocket.getPort(), "192.168.187.1", false, false, false, this.message.username);
                         this.users.addUser(u2);   
                         // inizializzo messaggio di risposta     
                         this.message = new Message(u2.isUno, CORRECT, u2.userName,"Username disponibile");
